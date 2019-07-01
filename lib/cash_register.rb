5e654df32b97f98f0631a4832f0035f97360d32f
Connor Cowan <ccowan003@comcast.net>
2019-07-01 23:43:00 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -3,7 +3,7 @@ class CashRegister
   attr_accessor :items, :discount, :total, :last_transaction        #ATTRIBUTES
 
   def initialize(discount = 0)
-    @total = 0
+    @total = 0                        #set total initially to 0
     @discount = discount
     @items = []       #set items attribute to an item array
   end
